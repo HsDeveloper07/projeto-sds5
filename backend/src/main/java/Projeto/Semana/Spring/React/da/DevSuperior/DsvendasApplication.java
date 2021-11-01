b605e9fc9d2241e96279379db388f0d1d0497fca
Henrique Santos <hsdeveloper07@gmail.com>
2021-11-01 20:00:44 -0300

    Project Spring React - Create

---
+++ backend/src/main/java/Projeto/Semana/Spring/React/da/DevSuperior/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package Projeto.Semana.Spring.React.da.DevSuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Projeto/Semana/Spring/React/da/DevSuperior/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.Semana.Spring.React.da.DevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
